ad9ae014ba25fcef5bf391eaa159c6d83180d802
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2015-06-08 00:29:08 +0300

    FSM Back Events

--- ZSM/FSM.cs
+++ ZSM/FSM.cs
@@ -4,168 +4,13 @@ using System.Collections.Generic;
 
 namespace ZSM{
 
-	public interface IState {
-		FSM Parent { get; set; }
-		Dictionary<string, object> Data { get; }
-		string Enter(params object[] args);
-		string Do(params object[] args);
-		string Exit(params object[] args);
-	}
-
-	public class State : IState{
-
-		public FSM Parent { get; set; }
-
-		public Dictionary<string, object> Data { get; protected set; }
-
-		public virtual string Enter(params object[] args) {
-			return "";
-		}
-
-		public virtual string Do(params object[] args){
-			return "";
-		}
-
-		public virtual string Exit(params object[] args) {
-			return "";
-		}
-
-		public State() {
-			Data = new Dictionary<string, object>();
-		}
-
-		public State(Dictionary<string, object> data) {
-			Data = data;
-		}
-	}
-
-	public class SmartState : IState {
-
-		private Route route;
-		public FSM Parent {get; set; }
-		public Dictionary<string, object> Data { get; protected set; }
-
-		public int GetInt(string name){
-			return GetInt(name, 0);
-		}
-
-		public int GetInt(string name, int def){
-			if (!Data.ContainsKey(name))
-				return def;
-			else
-				return (int)Data[name];
-		}
-
-		public float GetFloat(string name){
-			return GetFloat(name, 0);
-		}
-
-		public float GetFloat(string name, float def){
-			if (!Data.ContainsKey(name))
-				return def;
-			else
-				return (float)Data[name];
-		}
-
-		public string GetString(string name){
-			return GetString(name, "");
-		}
-
-		public string GetString(string name, string def){
-			if (!Data.ContainsKey(name))
-				return def;
-			else
-				return (string)Data[name];
-		}
-
-		public bool GetBool(string name){
-			return GetBool(name, false);
-		}
-
-		public bool GetBool(string name, bool def){
-			if (!Data.ContainsKey(name))
-				return def;
-			else
-				return (bool)Data[name];
-		}
-
-		public double GetDouble(string name){
-			return GetDouble(name, 0);
-		}
-
-		public double GetDouble(string name, double def){
-			if (!Data.ContainsKey(name))
-				return def;
-			else
-				return (double)Data[name];
-		}
-
-		public void Inc(string name){
-			int i = GetInt(name, 0);
-			this.Data[name] = i + 1;
-		}
-
-		public void Inc(string name, int val){
-			int i = GetInt(name, 0);
-			this.Data[name] = i + val;
-		}
-
-		public void Inc(string name, float val){
-			float i = GetFloat(name, 0);
-			this.Data[name] = i + val;
-		}
-
-
-		public void Dec(string name){
-			int i = GetInt(name, 0);
-			this.Data[name] = i - 1;
-		}
-
-		public void Dec(string name, int val){
-			int i = GetInt(name, 0);
-			this.Data[name] = i - val;
-		}
-
-		public void Dec(string name, float val){
-			float i = GetFloat(name, 0);
-			this.Data[name] = i - val;
-		}
-
-
-		public virtual string Enter(params object[] args) {
-			return "";
-		}
-
-		public virtual string Do(params object[] args) {
-			if (args.Length > 0)
-				return route.Do(args);
-			return "";
-		}
-
-		public virtual string Exit(params object[] args) {
-			return "";
-		}
-
-		public SmartState():this(new Dictionary<string, object>()) {}
-
-		public SmartState(Dictionary<string, object> data) {
-			Data = data;
-			route = new Route(this);
-		}
-	}
-
-
-	[AttributeUsage(AttributeTargets.Class)]
-	public class StateAttribute : Attribute {
-		public string Name { get; set; }
-		public StateAttribute(string name){
-			this.Name = name;
-		}
-	}
+	public delegate void DFSMEvent(FSM fms, string method, object[] args);
 
 	public class FSM {
 
 		public Dictionary<string, IState> states;
+		public Dictionary<string, List<DFSMEvent>> events;
+
 		public IState CurrentState { get; protected set; }
 
 		public void Add(IState state) {
@@ -178,6 +23,23 @@ namespace ZSM{
 
 			this.Add(state.GetType().Name, state);
 		}
+
+		public void AddEvent(string eventName, DFSMEvent target){
+			if (!this.events.ContainsKey(eventName))
+				this.events.Add(eventName, new List<DFSMEvent>());	
+				
+			this.events[eventName].Add(target);
+			
+		}
+
+		public void RemoveEvent(string eventName, DFSMEvent target){
+			if (!this.events.ContainsKey(eventName))
+				return;
+			this.events[eventName].Remove(target);
+			if (this.events[eventName].Count == 0)
+				this.events.Remove(eventName);
+		}
+
 
 		public void Add(string name, IState state) {
 			state.Parent = this;
@@ -223,9 +85,11 @@ namespace ZSM{
 
 		public FSM() {
 			states = new Dictionary<string, IState>();
+			events = new Dictionary<string, List<DFSMEvent>>();
 		}
 
 	}
+
 
 
 
---
+++ ZSM/State.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZSM {
+	public interface IState {
+		FSM Parent { get; set; }
+		Dictionary<string, object> Data { get; }
+		string Enter(params object[] args);
+		string Do(params object[] args);
+		string Exit(params object[] args);
+	}
+
+	public class State : IState{
+
+		public FSM Parent { get; set; }
+
+		public Dictionary<string, object> Data { get; protected set; }
+
+		public virtual string Enter(params object[] args) {
+			return "";
+		}
+
+		public virtual string Do(params object[] args){
+			return "";
+		}
+
+		public virtual string Exit(params object[] args) {
+			return "";
+		}
+
+		public State() {
+			Data = new Dictionary<string, object>();
+		}
+
+		public State(Dictionary<string, object> data) {
+			Data = data;
+		}
+	}
+
+	public class SmartState : IState {
+
+		private Route route;
+		public FSM Parent {get; set; }
+		public Dictionary<string, object> Data { get; protected set; }
+
+		public int GetInt(string name){
+			return GetInt(name, 0);
+		}
+
+		public int GetInt(string name, int def){
+			if (!Data.ContainsKey(name))
+				return def;
+			else
+				return (int)Data[name];
+		}
+
+		public float GetFloat(string name){
+			return GetFloat(name, 0);
+		}
+
+		public float GetFloat(string name, float def){
+			if (!Data.ContainsKey(name))
+				return def;
+			else
+				return (float)Data[name];
+		}
+
+		public string GetString(string name){
+			return GetString(name, "");
+		}
+
+		public string GetString(string name, string def){
+			if (!Data.ContainsKey(name))
+				return def;
+			else
+				return (string)Data[name];
+		}
+
+		public bool GetBool(string name){
+			return GetBool(name, false);
+		}
+
+		public bool GetBool(string name, bool def){
+			if (!Data.ContainsKey(name))
+				return def;
+			else
+				return (bool)Data[name];
+		}
+
+		public double GetDouble(string name){
+			return GetDouble(name, 0);
+		}
+
+		public double GetDouble(string name, double def){
+			if (!Data.ContainsKey(name))
+				return def;
+			else
+				return (double)Data[name];
+		}
+
+		public void Inc(string name){
+			int i = GetInt(name, 0);
+			this.Data[name] = i + 1;
+		}
+
+		public void Inc(string name, int val){
+			int i = GetInt(name, 0);
+			this.Data[name] = i + val;
+		}
+
+		public void Inc(string name, float val){
+			float i = GetFloat(name, 0);
+			this.Data[name] = i + val;
+		}
+
+
+		public void Dec(string name){
+			int i = GetInt(name, 0);
+			this.Data[name] = i - 1;
+		}
+
+		public void Dec(string name, int val){
+			int i = GetInt(name, 0);
+			this.Data[name] = i - val;
+		}
+
+		public void Dec(string name, float val){
+			float i = GetFloat(name, 0);
+			this.Data[name] = i - val;
+		}
+
+
+		public virtual string Enter(params object[] args) {
+			return "";
+		}
+
+		public virtual string Do(params object[] args) {
+			if (args.Length > 0)
+				return route.Do(args);
+			return "";
+		}
+
+		public virtual string Exit(params object[] args) {
+			return "";
+		}
+
+		public SmartState():this(new Dictionary<string, object>()) {}
+
+		public SmartState(Dictionary<string, object> data) {
+			Data = data;
+			route = new Route(this);
+		}
+	}
+
+
+	[AttributeUsage(AttributeTargets.Class)]
+	public class StateAttribute : Attribute {
+		public string Name { get; set; }
+		public StateAttribute(string name){
+			this.Name = name;
+		}
+	}
+}
+
